0ac544eea479d0c901bf5720c46b43913bd01fb3
MarkusK <MKauschke@gmx.de>
2017-07-11 19:54:57 +0200

    Added some more error messages and did some cleanup. Prepared for Cleverbot but Nuget lib is not working yet.

---
+++ MonkeyBot/Modules/CleverBotModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Threading.Tasks;
+//using Cleverbot.Net;
+
+namespace MonkeyBot.Modules
+{
+    public class CleverBotModule : ModuleBase
+    {
+        //[Command("cleverbot"), Summary("Tells you who you are")]
+        //public async Task Cleverbot([Remainder, Summary("The text to send to cleverbot")] string text)
+        //{
+        //    CleverbotSession cleverbot = new CleverbotSession("CC3874vDv932cueUJ0yBviCdimw");
+        //    CleverbotResponse r = await cleverbot.GetResponseAsync("text");
+        //    await ReplyAsync(r.Response);
+        //}
+    }
+}
