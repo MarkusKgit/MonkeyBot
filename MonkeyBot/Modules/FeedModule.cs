fc3601c0ad81545c3e307eaa282ce9c23d572678
Markus K <13160892+MarkusKgit@users.noreply.github.com>
2018-04-04 08:17:55 +0200

    Moved feeds to own module

---
+++ MonkeyBot/Modules/FeedModule.cs
@@ -0,0 +1,126 @@
+﻿using CodeHollow.FeedReader;
+using Discord;
+using Discord.Commands;
+using dokas.FluentStrings;
+using Microsoft.Extensions.Logging;
+using MonkeyBot.Common;
+using MonkeyBot.Preconditions;
+using MonkeyBot.Services;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MonkeyBot.Modules
+{
+    [Group("Feeds")]
+    [Name("Feeds")]
+    [MinPermissions(AccessLevel.ServerAdmin)]
+    [RequireContext(ContextType.Guild)]
+    public class FeedModule : ModuleBase
+    {
+        private readonly DbService dbService;
+        private readonly IFeedService feedService;
+
+        public FeedModule(DbService dbService, IFeedService backgroundService)
+        {
+            this.dbService = dbService;
+            this.feedService = backgroundService;
+        }
+
+        [Command("Add")]
+        [Remarks("Adds an atom or RSS feed to the list of listened feeds.")]
+        public async Task AddFeedUrlAsync([Summary("The url to the feed (Atom/RSS)")] string url, [Summary("Optional: The name of the channel where the Feed updates should be posted. Defaults to current channel")] string channelName = "")
+        {
+            if (url.IsEmpty())
+            {
+                await ReplyAsync("Please enter a feed url!");
+                return;
+            }
+            var allChannels = await Context.Guild.GetTextChannelsAsync();
+            ITextChannel channel;
+            if (!channelName.IsEmpty())
+            {
+                channel = allChannels.FirstOrDefault(x => x.Name.ToLower() == channelName.ToLower());
+                if (channel == null)
+                {
+                    await ReplyAsync("The specified channel does not exist");
+                    return;
+                }
+            }
+            else
+            {
+                channel = Context.Channel as ITextChannel;
+            }
+            var urls = await FeedReader.GetFeedUrlsFromUrlAsync(url);
+            string feedUrl;
+            if (urls.Count() < 1) // no url - probably the url is already the right feed url
+                feedUrl = url;
+            else if (urls.Count() == 1)
+                feedUrl = urls.First().Url;
+            else
+            {
+                await ReplyAsync($"Multiple feeds were found at this url. Please be more specific:{Environment.NewLine}{string.Join(Environment.NewLine, urls)}");
+                return;
+            }
+            using (var uow = dbService.UnitOfWork)
+            {
+                var config = await uow.GuildConfigs.GetAsync(Context.Guild.Id);
+                if (config == null)
+                    config = new GuildConfig(Context.Guild.Id);
+                if (config.FeedUrls.Contains(feedUrl))
+                {
+                    await ReplyAsync("The specified feed is already in the list!");
+                    return;
+                }
+                config.FeedUrls.Add(feedUrl);
+                await uow.GuildConfigs.AddOrUpdateAsync(config);
+                await uow.CompleteAsync();
+                await ReplyAsync("Feed added");
+            }
+            await feedService.RunOnceSingleFeedAsync(Context.Guild.Id, Context.Channel.Id, feedUrl, true);
+        }
+
+        [Command("Remove")]
+        [Remarks("Removes the specified feed from the list of feeds.")]
+        public async Task RemoveFeedUrlAsync([Summary("The url of the feed")][Remainder] string url)
+        {
+            if (url.IsEmpty())
+            {
+                await ReplyAsync("Please enter a feed url");
+                return;
+            }
+            using (var uow = dbService.UnitOfWork)
+            {
+                var config = await uow.GuildConfigs.GetAsync(Context.Guild.Id);
+                if (config == null)
+                    config = new GuildConfig(Context.Guild.Id);
+                if (!config.FeedUrls.Contains(url))
+                {
+                    await ReplyAsync("The specified feed is not in the list!");
+                    return;
+                }
+                config.FeedUrls.Remove(url);
+                await uow.GuildConfigs.AddOrUpdateAsync(config);
+                await uow.CompleteAsync();
+                await ReplyAsync("Feed removed");
+            }
+        }
+
+        [Command("RemoveAll")]
+        [Remarks("Removes all feed urls")]
+        public async Task RemoveFeedUrlsAsync()
+        {
+            using (var uow = dbService.UnitOfWork)
+            {
+                var config = await uow.GuildConfigs.GetAsync(Context.Guild.Id);
+                if (config != null)
+                {
+                    config.FeedUrls.Clear();
+                    await uow.GuildConfigs.AddOrUpdateAsync(config);
+                    await uow.CompleteAsync();
+                }
+            }
+        }
+    }
+}
--- MonkeyBot/Modules/GuildConfigModule.cs
+++ MonkeyBot/Modules/GuildConfigModule.cs
@@ -1,11 +1,8 @@
-﻿using CodeHollow.FeedReader;
-using Discord;
-using Discord.Commands;
+﻿using Discord.Commands;
 using dokas.FluentStrings;
 using MonkeyBot.Common;
 using MonkeyBot.Preconditions;
 using MonkeyBot.Services;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,12 +14,10 @@ namespace MonkeyBot.Modules
     public class GuildConfigModule : ModuleBase
     {
         private readonly DbService dbService;
-        private readonly IFeedService backgroundService;
 
-        public GuildConfigModule(DbService db, IFeedService backgroundService)
+        public GuildConfigModule(DbService db)
         {
             this.dbService = db;
-            this.backgroundService = backgroundService;
         }
 
         #region WelcomeMessage
@@ -92,131 +87,5 @@ namespace MonkeyBot.Modules
         }
 
         #endregion Rules
-
-        #region Feeds
-
-        [Command("AddFeedUrl")]
-        [Remarks("Adds an atom or RSS feed to the list of listened feeds.")]
-        public async Task AddFeedUrlAsync([Summary("The url to the feed (Atom/RSS)")][Remainder] string url)
-        {
-            if (url.IsEmpty())
-            {
-                await ReplyAsync("Please enter a feed url");
-                return;
-            }
-            var urls = await FeedReader.GetFeedUrlsFromUrlAsync(url);
-            string feedUrl;
-            if (urls.Count() < 1) // no url - probably the url is already the right feed url
-                feedUrl = url;
-            else if (urls.Count() == 1)
-                feedUrl = urls.First().Url;
-            else
-            {
-                await ReplyAsync($"Multiple feeds were found at this url. Please be more specific:{Environment.NewLine}{string.Join(Environment.NewLine, urls)}");
-                return;
-            }
-            using (var uow = dbService.UnitOfWork)
-            {
-                var config = await uow.GuildConfigs.GetAsync(Context.Guild.Id);
-                if (config == null)
-                    config = new GuildConfig(Context.Guild.Id);
-                if (config.FeedUrls.Contains(feedUrl))
-                {
-                    await ReplyAsync("The specified feed is already in the list!");
-                    return;
-                }
-                config.FeedUrls.Add(feedUrl);
-                await uow.GuildConfigs.AddOrUpdateAsync(config);
-                await uow.CompleteAsync();
-                await ReplyAsync("Feed added");
-            }
-            await backgroundService.RunOnceSingleFeedAsync(Context.Guild.Id, Context.Channel.Id, feedUrl, true);
-        }
-
-        [Command("RemoveFeedUrl")]
-        [Remarks("Removes the specified feed from the list of feeds.")]
-        public async Task RemoveFeedUrlAsync([Summary("The url of the feed")][Remainder] string url)
-        {
-            if (url.IsEmpty())
-            {
-                await ReplyAsync("Please enter a feed url");
-                return;
-            }
-            using (var uow = dbService.UnitOfWork)
-            {
-                var config = await uow.GuildConfigs.GetAsync(Context.Guild.Id);
-                if (config == null)
-                    config = new GuildConfig(Context.Guild.Id);
-                if (!config.FeedUrls.Contains(url))
-                {
-                    await ReplyAsync("The specified feed is not in the list!");
-                    return;
-                }
-                config.FeedUrls.Remove(url);
-                await uow.GuildConfigs.AddOrUpdateAsync(config);
-                await uow.CompleteAsync();
-                await ReplyAsync("Feed removed");
-            }
-        }
-
-        [Command("RemoveFeedUrls")]
-        [Remarks("Removes all feed urls")]
-        public async Task RemoveFeedUrlsAsync()
-        {
-            using (var uow = dbService.UnitOfWork)
-            {
-                var config = await uow.GuildConfigs.GetAsync(Context.Guild.Id);
-                if (config != null)
-                {
-                    config.FeedUrls.Clear();
-                    await uow.GuildConfigs.AddOrUpdateAsync(config);
-                    await uow.CompleteAsync();
-                }
-            }
-        }
-
-        [Command("EnableFeeds")]
-        [Remarks("Enables the feed listener in the specified channel")]
-        [Example("!EnableFeeds general")]
-        public async Task EnableFeedsAsync([Summary("The channel where the feed updates should be broadcasted")]string channelName = "")
-        {
-            IGuildChannel channel;
-            if (channelName == "")
-                channel = Context.Channel as IGuildChannel;
-            else
-                channel = (await Context.Guild?.GetChannelsAsync())?.FirstOrDefault(x => x.Name == channelName);
-            if (channel == null)
-            {
-                await ReplyAsync("The specified channel does not exist");
-                return;
-            }
-            await ToggleFeedsInternalAsync(true, channel.Id);
-            await backgroundService?.RunOnceAllFeedsAsync(Context.Guild.Id);
-        }
-
-        [Command("DisableFeeds")]
-        [Remarks("Disables the feed listener")]
-        public async Task DisableFeedsAsync()
-        {
-            await ToggleFeedsInternalAsync(false);
-        }
-
-        private async Task ToggleFeedsInternalAsync(bool enable, ulong channelID = 0)
-        {
-            using (var uow = dbService.UnitOfWork)
-            {
-                var config = await uow.GuildConfigs.GetAsync(Context.Guild.Id);
-                if (config == null)
-                    config = new GuildConfig(Context.Guild.Id);
-                config.ListenToFeeds = enable;
-                if (enable)
-                    config.FeedChannelId = channelID;
-                await uow.GuildConfigs.AddOrUpdateAsync(config);
-                await uow.CompleteAsync();
-            }
-            await ReplyAsync($"Feeds have been {(enable ? "enabled" : "disabled.")}");
-        }
-
-        #endregion Feeds
     }
 }
