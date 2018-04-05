3156cf0ce5f0eee5202e4d4419fe9d4e0aaff424
Markus K <13160892+MarkusKgit@users.noreply.github.com>
2018-04-05 14:21:51 +0200

    Added command to get Message Id

--- MonkeyBot/Modules/InfoModule.cs
+++ MonkeyBot/Modules/InfoModule.cs
@@ -1,7 +1,9 @@
 ﻿using Discord;
 using Discord.Commands;
+using dokas.FluentStrings;
 using MonkeyBot.Services;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MonkeyBot.Modules
@@ -36,6 +38,35 @@ namespace MonkeyBot.Modules
                 builder.AddField($"Rules of {Context.Guild.Name}:", string.Join(Environment.NewLine, rules));
                 await Context.User.SendMessageAsync("", false, builder.Build());
                 await ReplyAsync("I have sent you a private message");
+            }
+        }
+
+        [Command("FindMessageID")]
+        [Remarks("Gets the message id of a message in the current channel with the provided message text")]
+        [RequireContext(ContextType.Guild)]
+        public async Task FindMessageIDAsync([Summary("The content of the message to search for")][Remainder] string messageContent)
+        {
+            if (messageContent.IsEmpty().OrWhiteSpace())
+            {
+                await ReplyAsync("You need to specify the text of the message to search for");
+                return;
+            }
+            const int searchDepth = 100;
+            var messages = await Context.Channel.GetMessagesAsync(searchDepth).Flatten();
+            var matches = messages.Where(x => x.Content.ToLowerInvariant().StartsWith(messageContent.ToLowerInvariant().Trim()));
+            if (matches == null || matches.Count() < 1)
+            {
+                await ReplyAsync($"Message not found. Hint: Only the last {searchDepth} messages in this channel are scanned.");
+                return;
+            }
+            else if (matches.Count() > 1)
+            {
+                await ReplyAsync($"{matches.Count()} Messages found. Please be more specific");
+                return;
+            }
+            else
+            {
+                await ReplyAsync($"The message Id is: {matches.First().Id}");
             }
         }
     }
