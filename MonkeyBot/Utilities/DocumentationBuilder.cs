2fb6ddb66eb23ecd72d2d7da65e551de7a75b0cd
Markus K <13160892+MarkusKgit@users.noreply.github.com>
2018-04-06 10:19:07 +0200

    Update Docu builder to Translate Guild and Channel Permissions

--- MonkeyBot/Utilities/DocumentationBuilder.cs
+++ MonkeyBot/Utilities/DocumentationBuilder.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using dokas.FluentStrings;
 using MonkeyBot.Common;
 using MonkeyBot.Modules;
@@ -74,29 +75,167 @@ namespace MonkeyBot.Utilities
                 return $"Minimum permission: {f.Em($"{(precondition as MinPermissionsAttribute).AccessLevel.ToString()}")}";
             else if (precondition is RequireContextAttribute)
             {
-                string context = string.Empty;
                 var contextAttribute = precondition as RequireContextAttribute;
-                switch (contextAttribute.Contexts)
-                {
-                    case ContextType.Guild:
-                        context = "channel";
-                        break;
-
-                    case ContextType.DM:
-                        context = "private message";
-                        break;
-
-                    case ContextType.Group:
-                        context = "private group";
-                        break;
-
-                    default:
-                        break;
-                }
+                string context = TranslateContext(contextAttribute.Contexts);
                 return $"Can only be used in a {f.Em(context)}";
+            }
+            else if (precondition is RequireBotPermissionAttribute || precondition is RequireUserPermissionAttribute)
+            {
+                string permission = "";
+                GuildPermission? guildPermission;
+                ChannelPermission? channelPermission;
+                if (precondition is RequireBotPermissionAttribute)
+                {
+                    guildPermission = (precondition as RequireBotPermissionAttribute).GuildPermission;
+                    channelPermission = (precondition as RequireBotPermissionAttribute).ChannelPermission;
+                }
+                else
+                {
+                    guildPermission = (precondition as RequireUserPermissionAttribute).GuildPermission;
+                    channelPermission = (precondition as RequireUserPermissionAttribute).ChannelPermission;
+                }
+                if (guildPermission != null && guildPermission.HasValue)
+                {
+                    permission += $"Requires guild permission: {f.Em(TranslateGuildPermission(guildPermission.Value))} ";
+                }
+                if (channelPermission != null && channelPermission.HasValue)
+                {
+                    permission += $"Requires channel permission: {f.Em(TranslateChannelPermission(channelPermission.Value))}";
+                }
+                return permission.Trim();
             }
             else
                 return precondition.ToString();
+        }
+
+        private static string TranslateContext(ContextType context)
+        {
+            switch (context)
+            {
+                case ContextType.Guild:
+                    return "channel";
+
+                case ContextType.DM:
+                    return "private message";
+
+                case ContextType.Group:
+                    return "private group";
+
+                default:
+                    return "";
+            }
+        }
+
+        private static string TranslateGuildPermission(GuildPermission guildPermission)
+        {
+            switch (guildPermission)
+            {
+                case GuildPermission.CreateInstantInvite:
+                    return "Create Instant Invite";
+                case GuildPermission.KickMembers:
+                    return "Kick Members";
+                case GuildPermission.BanMembers:
+                    return "Ban Members";
+                case GuildPermission.Administrator:
+                    return "Administrator";
+                case GuildPermission.ManageChannels:
+                    return "Manage Channels";
+                case GuildPermission.ManageGuild:
+                    return "Manage Guild";
+                case GuildPermission.AddReactions:
+                    return "Add Reactions";
+                case GuildPermission.ReadMessages:
+                    return "Read Messages";
+                case GuildPermission.SendMessages:
+                    return "Send Messages";
+                case GuildPermission.SendTTSMessages:
+                    return "Send TTS Messages";
+                case GuildPermission.ManageMessages:
+                    return "Manage Messages";
+                case GuildPermission.EmbedLinks:
+                    return "Embed Links";
+                case GuildPermission.AttachFiles:
+                    return "Attach Files";
+                case GuildPermission.ReadMessageHistory:
+                    return "Read Message History";
+                case GuildPermission.MentionEveryone:
+                    return "Mention Everyone";
+                case GuildPermission.UseExternalEmojis:
+                    return "Use External Emojis";
+                case GuildPermission.Connect:
+                    return "Connect";
+                case GuildPermission.Speak:
+                    return "Speak";
+                case GuildPermission.MuteMembers:
+                    return "Mute Members";
+                case GuildPermission.DeafenMembers:
+                    return "Deafen Members";
+                case GuildPermission.MoveMembers:
+                    return "Move Members";
+                case GuildPermission.UseVAD:
+                    return "Use VAD";
+                case GuildPermission.ChangeNickname:
+                    return "Change Nickname";
+                case GuildPermission.ManageNicknames:
+                    return "Manage Nicknames";
+                case GuildPermission.ManageRoles:
+                    return "Manage Roles";
+                case GuildPermission.ManageWebhooks:
+                    return "Manage Webhooks";
+                case GuildPermission.ManageEmojis:
+                    return "Manage Emojis";
+                default:
+                    return guildPermission.ToString();
+            }
+        }
+
+        private static string TranslateChannelPermission(ChannelPermission channelPermission)
+        {
+            switch (channelPermission)
+            {
+                case ChannelPermission.CreateInstantInvite:
+                    return "Create Instant Invite";
+                case ChannelPermission.ManageChannel:
+                    return "Manage Channel";
+                case ChannelPermission.AddReactions:
+                    return "Add Reactions";
+                case ChannelPermission.ReadMessages:
+                    return "Read Messages";
+                case ChannelPermission.SendMessages:
+                    return "Send Messages";
+                case ChannelPermission.SendTTSMessages:
+                    return "Send TTS Messages";
+                case ChannelPermission.ManageMessages:
+                    return "Manage Messages";
+                case ChannelPermission.EmbedLinks:
+                    return "Embed Links";
+                case ChannelPermission.AttachFiles:
+                    return "Attach Files";
+                case ChannelPermission.ReadMessageHistory:
+                    return "Read Message History";
+                case ChannelPermission.MentionEveryone:
+                    return "Mention Everyone";
+                case ChannelPermission.UseExternalEmojis:
+                    return "Use External Emojis";
+                case ChannelPermission.Connect:
+                    return "Connect";
+                case ChannelPermission.Speak:
+                    return "Speak";
+                case ChannelPermission.MuteMembers:
+                    return "Mute Members";
+                case ChannelPermission.DeafenMembers:
+                    return "Deafen Members";
+                case ChannelPermission.MoveMembers:
+                    return "Move Members";
+                case ChannelPermission.UseVAD:
+                    return "Use VAD";
+                case ChannelPermission.ManagePermissions:
+                    return "Manage Permissions";
+                case ChannelPermission.ManageWebhooks:
+                    return "Manage Webhooks";
+                default:
+                    return channelPermission.ToString();
+            }
         }
 
         public enum OutputTypes
