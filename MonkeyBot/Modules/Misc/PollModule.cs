94dfdda422a895906180364320587452688b4ccd
Markus K <13160892+MarkusKgit@users.noreply.github.com>
2020-09-01 12:08:40 +0200

    Poll overhaul

--- MonkeyBot/Modules/Misc/PollModule.cs
+++ MonkeyBot/Modules/Misc/PollModule.cs
@@ -6,6 +6,7 @@ using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Enums;
 using Fclp.Internals.Extensions;
 using Humanizer;
+using Microsoft.EntityFrameworkCore.Internal;
 using MonkeyBot.Common;
 using System;
 using System.Collections.Generic;
@@ -24,28 +25,21 @@ namespace MonkeyBot.Modules
     {
         private static InteractivityExtension interactivity;
         private static Chronic.Parser timeParser;
+        private static readonly DiscordEmoji okEmoji = DiscordEmoji.FromUnicode("👍");
+        private const int timeOutSeconds = 60;
+        private readonly TimeSpan timeOut = TimeSpan.FromSeconds(timeOutSeconds);
 
-        private static DiscordEmoji okEmoji = DiscordEmoji.FromUnicode("👍");
-        private static DiscordEmoji plusEmoji = DiscordEmoji.FromUnicode("➕");
-
-        //private static readonly Dictionary<DiscordEmoji, string> yesNoPollAnswers = new Dictionary<DiscordEmoji, string>()
-        //{
-        //    {DiscordEmoji.FromUnicode("👍"), "Yes" },
-        //    {DiscordEmoji.FromUnicode("👎"), "No" },
-        //    {DiscordEmoji.FromUnicode("🤷"), "Don't care" }
-        //};
-
-        private const string introText =
-            "I will now guide you through the creation of the poll with a set of instructions.\n" +
-            "Watch this message for updated steps \n" +
-            "Above you can see a preview of the poll that will get created \n\n";            
+        private readonly string introText =
+            $"I will now guide you through the creation of the poll with a set of instructions.\n" +
+            $"You have {timeOutSeconds} seconds to answer each question \n" +
+            $"Above you can see a preview of the poll that will get created \n\n";
 
         private const string firstInstruction = "**1. Type a poll question**";
         private const string secondInstruction = "**2. When should the poll end? Valid examples are:** \n" +
             "*17:00* \n" +
             "*Tomorrow 17:00* \n" +
             "*Monday 17:00* \n";
-        private const string thirdInstruction = "**3. By clicking on \"+\" you can add a possible answer to the poll. You will be prompted for the text each time. Once you are done click \"👍\" to start the poll**";
+        private const string thirdInstruction = "**3. Now enter at least 2 poll answers by sending a message for each answer. Once you are done click \"👍\" to start the poll**";
 
         [Command("Poll")]
         [Aliases("Vote")]
@@ -71,10 +65,7 @@ namespace MonkeyBot.Modules
 
             interactivity ??= ctx.Client.GetInteractivity();
 
-            var questionReponse = await interactivity.WaitForMessageAsync(msg =>
-            {
-                return msg.Author == ctx.Member && msg.ChannelId == ctx.Channel.Id;
-                }, TimeSpan.FromSeconds(60)).ConfigureAwait(false);
+            var questionReponse = await interactivity.WaitForMessageAsync(msg => msg.Author == ctx.Member && msg.ChannelId == ctx.Channel.Id, timeOut).ConfigureAwait(false);
             if (questionReponse.TimedOut)
             {
                 _ = await ctx.ErrorAsync("You didn't respond in time. Please start over", "Timed out").ConfigureAwait(false);
@@ -89,16 +80,15 @@ namespace MonkeyBot.Modules
                 return;
             }
 
-            pollEmbed.WithTitle($"**{pollQuestion}**");
+            pollEmbed.WithTitle($"**Poll: {pollQuestion}**");
             pollMessage = await pollMessage.ModifyAsync(embed: pollEmbed.Build()).ConfigureAwait(false);
 
+            await ctx.Channel.DeleteMessageAsync(questionReponse.Result).ConfigureAwait(false);
+
             setupEmbed.WithDescription(introText + secondInstruction);
-            setupMessage = await setupMessage.ModifyAsync(embed: setupEmbed.Build()).ConfigureAwait(false);
-            
-            var timeResponse = await interactivity.WaitForMessageAsync(msg =>
-            {
-                return msg.Author == ctx.Member && msg.ChannelId == ctx.Channel.Id;
-            }, TimeSpan.FromSeconds(60)).ConfigureAwait(false);
+            await setupMessage.ModifyAsync(embed: setupEmbed.Build()).ConfigureAwait(false);
+
+            var timeResponse = await interactivity.WaitForMessageAsync(msg => msg.Author == ctx.Member && msg.ChannelId == ctx.Channel.Id, timeOut).ConfigureAwait(false);
             if (timeResponse.TimedOut)
             {
                 _ = await ctx.ErrorAsync("You didn't respond in time. Please start over", "Timed out").ConfigureAwait(false);
@@ -114,116 +104,88 @@ namespace MonkeyBot.Modules
                 return;
             }
             DateTime endTime = parsedTime.ToTime();
+            if (endTime < DateTime.Now)
+            {
+                _ = await ctx.ErrorAsync("The provided time is in the past. Please start over", "Invalid Time").ConfigureAwait(false);
+                await ctx.Channel.DeleteMessagesAsync(new[] { setupMessage, pollMessage }).ConfigureAwait(false);
+                return;
+            }
 
-            pollEmbed.WithFooter($"Vote will end {endTime.ToUniversalTime()} UTC");
+            pollEmbed.WithFooter($"Poll will end on {endTime:dd.MM.yyyy} at {endTime:HH:mm \"UTC\"zz}");
             pollMessage = await pollMessage.ModifyAsync(embed: pollEmbed.Build()).ConfigureAwait(false);
 
+            await ctx.Channel.DeleteMessageAsync(timeResponse.Result).ConfigureAwait(false);
+
             setupEmbed.WithDescription(introText + thirdInstruction);
-            setupMessage = await setupMessage.ModifyAsync(embed: setupEmbed.Build()).ConfigureAwait(false);
-            await setupMessage.CreateReactionAsync(plusEmoji).ConfigureAwait(false);
+            await setupMessage.ModifyAsync(embed: setupEmbed.Build()).ConfigureAwait(false);
             await setupMessage.CreateReactionAsync(okEmoji).ConfigureAwait(false);
 
-            bool keepAdding = true;
-            List<string> pollAnswers = new List<string>();
-            while (keepAdding)
+            var pollAnswers = new Dictionary<DiscordEmoji, string>();
+            while (true)
             {
-                var reaction = await interactivity.WaitForReactionAsync(r => r.User == ctx.Member && r.Emoji == okEmoji || r.Emoji == plusEmoji).ConfigureAwait(false);
-                if (reaction.TimedOut || reaction.Result.Emoji == okEmoji)
+                var addAnswerTask = interactivity.WaitForMessageAsync(msg => msg.Author == ctx.Member && msg.ChannelId == ctx.Channel.Id, timeOut);
+                var okTask = interactivity.WaitForReactionAsync(r => r.User == ctx.Member && r.Emoji == okEmoji, timeOut);
+                var result = await Task.WhenAny(addAnswerTask, okTask).ConfigureAwait(false);
+
+                if (result == addAnswerTask)
                 {
-                    keepAdding = false;
+                    var pollOptionResponse = addAnswerTask.Result;
+                    if (pollOptionResponse.TimedOut)
+                    {
+                        if (pollAnswers.Count >= 2)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            _ = await ctx.ErrorAsync("You didn't provide enough answers in time. Please start over", "Timed out").ConfigureAwait(false);
+                            await ctx.Channel.DeleteMessagesAsync(new[] { setupMessage, pollMessage }).ConfigureAwait(false);
+                            return;
+                        }
+                    }
+                    pollAnswers.Add(DiscordEmoji.FromUnicode(MonkeyHelpers.GetUnicodeRegionalLetter(pollAnswers.Count)), pollOptionResponse.Result.Content);
+                    await ctx.Channel.DeleteMessageAsync(pollOptionResponse.Result).ConfigureAwait(false);
+                    pollEmbed.WithDescription(string.Join("\n", pollAnswers.Select(ans => $"{ans.Key} {ans.Value}")));
+                    pollMessage = await pollMessage.ModifyAsync(embed: pollEmbed.Build()).ConfigureAwait(false);
                 }
                 else
                 {
-                    await setupMessage.DeleteReactionAsync(plusEmoji, ctx.Member).ConfigureAwait(false);
+                    if (pollAnswers.Count < 2)
+                    {
+                        _ = await ctx.ErrorAsync("Not enough answer options! Please add more first!").ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
             }
+            await ctx.Channel.DeleteMessageAsync(setupMessage).ConfigureAwait(false);
+
+            foreach (DiscordEmoji pollEmoji in pollAnswers.Keys)
+            {
+                await pollMessage.CreateReactionAsync(pollEmoji).ConfigureAwait(false);
+            }
+            await pollMessage.PinAsync().ConfigureAwait(false);
+
+            TimeSpan pollDuration = endTime - DateTime.Now;
+            var pollResult = await interactivity.DoPollAsync(pollMessage, pollAnswers.Keys.ToArray(), PollBehaviour.KeepEmojis, pollDuration).ConfigureAwait(false);
+            await pollMessage.UnpinAsync().ConfigureAwait(false);
+            if (!pollResult.Any(x => x.Total > 0))
+            {
+                _ = await ctx.RespondAsync($"No one participated in the poll {pollQuestion} :(").ConfigureAwait(false);
+                return;
+            }
+            int totalVotes = pollResult.Sum(r => r.Total);
+            var pollResultEmbed = new DiscordEmbedBuilder()
+                .WithTitle($"Poll results: {pollQuestion}")
+                .WithColor(DiscordColor.Azure)
+                .WithDescription($"**{ctx.Member.Mention}{(ctx.Member.Username.EndsWith('s') ? "'" : "'s")} poll ended. Here are the results:**\n\n" +
+                string.Join("\n", pollAnswers
+                    .Select(ans => new {Answer = ans.Value, Votes = pollResult.Single(r => r.Emoji == ans.Key).Total})
+                    .Select(ans => $"**{ans.Answer}**: {"vote".ToQuantity(ans.Votes)} ({100.0 * ans.Votes / totalVotes:F1} %)"))
+                );
+            _ = await ctx.RespondAsync(embed: pollResultEmbed.Build()).ConfigureAwait(false);
         }
-
-        //[Command("Poll")]
-        //[Aliases("Vote")]
-        //[Priority(1)]
-        //[Description("Starts a new poll with the specified question and automatically adds reactions")]
-        //[Example("!poll \"Is MonkeyBot awesome?\"")]
-        //[RequireBotPermissions(Permissions.AddReactions | Permissions.ManageMessages)]
-        //public async Task StartPollAsync(CommandContext ctx, [Description("The question")][RemainingText] string question)
-        //{
-        //    question = question.Trim('\"');
-        //    if (question.IsEmpty())
-        //    {
-        //        _ = await ctx.RespondAsync("Please enter a question").ConfigureAwait(false);
-        //        return;
-        //    }
-
-        //    Dictionary<DiscordEmoji, string> possibleAnswers = yesNoPollAnswers;
-
-        //    await DoPollInternalAsync(ctx, question, possibleAnswers).ConfigureAwait(false);
-        //}        
-
-        //[Command("Poll")]
-        //[Aliases("Vote")]
-        //[Priority(2)]
-        //[Description("Starts a new poll with the specified question and the list answers and automatically adds reactions")]
-        //[Example("!poll \"How cool is MonkeyBot?\" \"supercool\" \"over 9000\" \"bruh...\"")]
-        //[RequireBotPermissions(Permissions.AddReactions | Permissions.ManageMessages)]
-        //public async Task StartPollAsync(CommandContext ctx, [Description("The question")] string question, [Description("The list of answers")] params string[] answers)
-        //{
-        //    if (answers == null || answers.Length <= 2)
-        //    {
-        //        _ = await ctx.ErrorAsync("Please provide at least 2 answers").ConfigureAwait(false);
-        //        return;
-        //    }
-        //    if (answers.Length > 7)
-        //    {
-        //        _ = await ctx.ErrorAsync("Please provide a maximum of 7 answers").ConfigureAwait(false);
-        //        return;
-        //    }
-        //    question = question.Trim('\"');
-        //    if (question.IsEmptyOrWhiteSpace())
-        //    {
-        //        _ = await ctx.ErrorAsync("Please enter a question").ConfigureAwait(false);
-        //        return;
-        //    }
-
-        //    var possibleAnswers = answers.Select((ans, i) => (DiscordEmoji.FromUnicode(MonkeyHelpers.GetUnicodeRegionalLetter(i)), ans)).ToDictionary(x => x.Item1, x => x.ans);
-
-        //    await DoPollInternalAsync(ctx, question, possibleAnswers).ConfigureAwait(false);
-        //}
-
-        //private async Task DoPollInternalAsync(CommandContext ctx, string question, Dictionary<DiscordEmoji, string> possibleAnswers)
-        //{
-        //    var embedBuilder = new DiscordEmbedBuilder()
-        //                    .WithTitle($"New Poll: {question}")
-        //                    .WithColor(new DiscordColor(20, 20, 20))
-        //                    .WithDescription(
-        //                        "- Pick an option by clicking on the corresponding Emoji" + Environment.NewLine
-        //                        + "- You can only select one option" + Environment.NewLine
-        //                        + $"- You have {pollDuration.Humanize()} to cast your vote"
-        //                        )
-        //                    .AddField("Pick one", string.Join("\n", possibleAnswers.Select(x => $"{x.Key}: {x.Value}")));
-
-        //    DiscordMessage m = await ctx.RespondAsync(embed: embedBuilder.Build()).ConfigureAwait(false);
-
-        //    interactivity ??= ctx.Client.GetInteractivity();
-
-        //    var pollResult = await interactivity.DoPollAsync(m, possibleAnswers.Keys.ToArray(), PollBehaviour.DeleteEmojis, pollDuration).ConfigureAwait(false);
-
-        //    IEnumerable<string> answerCounts = possibleAnswers.Select(x => $"{x.Value}: {pollResult.SingleOrDefault(pr => pr.Emoji == x.Key)?.Total ?? 0}");
-
-        //    IEnumerable<string> participants = pollResult.SelectMany(x => x.Voted).Select(x => x.Mention).Distinct();
-
-        //    if (!participants.Any())
-        //    {
-        //        _ = await ctx.RespondAsync($"Not a single person voted on \"{question}\"").ConfigureAwait(false);
-        //        return;
-        //    }
-        //    embedBuilder = new DiscordEmbedBuilder()
-        //        .WithTitle($"Poll \"{question}\" ended")
-        //        .WithColor(new DiscordColor(20, 20, 20))
-        //        .AddField("Results", string.Join('\n', answerCounts))
-        //        .AddField("Voters", string.Join(", ", participants));
-
-        //    _ = await ctx.RespondAsync(embed: embedBuilder.Build()).ConfigureAwait(false);
-        //}
-
     }
 }
